37a0244484378497146a250d7211efc604758736
NTDat-MARS291205 <datksntalang117@gmail.com>
2025-09-23 22:32:44 +0700

    23/09/2025

--- Code_PTIT/main.py
+++ Code_PTIT/main.py
@@ -1,19 +1,19 @@
-import math
+from math import gcd
+class PhanSo:
+    def __init__(self, tu, mau):
+        ucln = gcd(tu, mau)
+        self.tu = tu // ucln
+        self.mau = mau // ucln
+    
+    def add(self, other):
+        self.tu = self.tu * other.mau + other.tu * self.mau
+        self.mau = self.mau * other.mau
+        return PhanSo(self.tu, self.mau)
 
-def convert(s):
-    sum = 0
-    if s[0] == '1':
-        sum += 4
-    if s[1] == '1':
-       sum += 2
-    if s[2] == '1':
-        sum += 1
-    return str(sum)
-
-num = input()
-num = num.zfill(len(num) + (3 - len(num) % 3) % 3)
-res = ""
-for i in range(0, len(num), 3):
-    tmp = num[i:i+3]
-    res += convert(tmp)
-print(res)
+    def __str__(self):
+        return f"{self.tu}/{self.mau}"
+    
+tu1, mau1, tu2, mau2 = map(int, input().split())
+p1 = PhanSo(tu1, mau1)
+p2 = PhanSo(tu2, mau2)
+print(p1.add(p2))
