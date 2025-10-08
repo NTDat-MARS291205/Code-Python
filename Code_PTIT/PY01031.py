084f7381ae125d779684c015503c6f5d36f09b09
NTDat-MARS291205 <datksntalang117@gmail.com>
2025-10-08 08:59:00 +0700

    08/10/2025

--- Code_PTIT/PY01031.py
+++ Code_PTIT/PY01031.py
@@ -3,12 +3,12 @@ for _ in range(tc):
     n, b = map(int, input().split())
     s = ""
     while n > 0:
-        d = n % b
+        du = n % b
         n = n // b
         if d < 10:
             s += str(d)
         else:
-            tmp = ord("A") + (d - 10)
+            tmp = ord("A") + (du - 10)
             s += chr(tmp)
     print(s[::-1])
 # 148
